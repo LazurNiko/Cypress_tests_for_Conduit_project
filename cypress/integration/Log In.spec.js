5d295b205f81e2e1f30a23f06274ee1ed863858e
Niko Lazur <74565339+LazurNiko@users.noreply.github.com>
2021-12-07 15:29:02 +0200

    added Page Object examples for Log in / Log out

--- cypress/integration/Log In.spec.js
+++ cypress/integration/Log In.spec.js
@@ -1,28 +1,14 @@
+import LogInObj from './pageObj/login'
+
 describe('Log In', () => {
+  const login = new LogInObj();
 
   it('User should succesfull login', () => {
     cy.visit('/');
     cy.get('[href="#login"]')
       .click();
-    cy.get('[placeholder="Email"]')
-      .type('niko12@mail.com');
-    cy.get('[placeholder="Password"]')
-      .type('12345Qwert!');
-    cy.get('.btn')
-      .click();
+    login.email().type('niko12@mail.com');
+    login.password().type('12345Qwert!');
+    login.signInBtn().should('be.visible').click();
   });
-
-    before(() => {
-        cy.login();
-        cy.visit('/');
-      });
-  
-    it('User should have ability to logout', () => {
-      cy.get('[href="#settings"]')
-        .click();
-        cy.url()
-        .should('include','/settings');
-      cy.contains('.btn', 'Or click here to logout.')
-        .click();
-    });
-})
+});
